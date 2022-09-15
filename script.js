7de01b1b540bc0e697909e419cf6f00d666869aa
Imruz360 <imruz2001@gmail.com>
2022-09-16 04:03:27 +0600

    update

--- script.js
+++ script.js
@@ -57,8 +57,11 @@ const typeController = (e) => {
 const validate = (key) => {
   if (key === questionText[userText.length - 1]) {
     return true;
+  } else {
+    errorCount++;
+    return false;
   }
-  return false;
+
 };
 
 // FINISHED TYPING
@@ -67,7 +70,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
