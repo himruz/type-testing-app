3a969d3cb561bb4425f77cfb9dbe031d7937a5a3
Imruz360 <imruz2001@gmail.com>
2022-09-16 21:50:20 +0600

    update

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
@@ -58,8 +59,7 @@ const validate = (key) => {
   if (key === questionText[userText.length - 1]) {
     return true;
   } else {
-    errorCount++;
-    // return false;
+    return false;
   }
 
 };
